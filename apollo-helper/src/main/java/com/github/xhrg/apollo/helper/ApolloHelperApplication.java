4c8563c8043ca275ab9f8a5cfe2f09b23c1d3329
xhrg <634789257@qq.com>
2022-08-24 14:50:27 +0800

    init project

---
+++ apollo-helper/src/main/java/com/github/xhrg/apollo/helper/ApolloHelperApplication.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.apollo.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloHelperApplication.class, args);
+	}
+
+}
